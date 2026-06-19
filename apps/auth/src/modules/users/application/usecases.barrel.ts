75ef67430f21d63ecc7f7ceb953390f508b4b62b
Gonzalo Bertomeu <gbertomeu@colegium.com>
2026-06-19 17:41:18 -0300

    Barrels pattern

---
+++ apps/auth/src/modules/users/application/usecases.barrel.ts
@@ -0,0 +1,4 @@
+import { CreateUserUseCase } from './usecases/CreateUser.usecase';
+import { GetUserUseCase } from './usecases/GetUser.usecase';
+
+export const useCases = [CreateUserUseCase, GetUserUseCase];
--- apps/auth/src/modules/users/infrastructure/users.module.ts
+++ apps/auth/src/modules/users/infrastructure/users.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './persistence/User.entity';
+import { useCases } from '../application/usecases.barrel';
+import { UserRepository } from '../domain/repositories/User.repository';
+import { UserRepositoryOrm } from './persistence/User.repository-orm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [
+    {
+      provide: UserRepository,
+      useClass: UserRepositoryOrm,
+    },
+    ...useCases,
+  ],
+  exports: [...useCases],
 })
 export class UsersModule {}
